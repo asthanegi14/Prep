7a2c4b6ede0fa1041073777427d4e1b6dd15e06e
Astha Negi <asthanegi539@gmail.com>
2023-07-11 20:46:28 +0530

    Create Reverse a String.java

---
+++ Arrays/Reverse a String.java
@@ -0,0 +1,14 @@
+class Reverse
+{
+    // Complete the function
+    // str: input string
+    public static String reverseWord(String str)
+    {
+        // Reverse the string str
+        StringBuffer sb = new StringBuffer();
+        for(int i=str.length()-1;i>=0;i--){
+            sb.append(str.charAt(i));
+        }
+        return sb.toString();
+    }
+}
